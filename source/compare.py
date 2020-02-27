9b378a7774d92e72a88bc824ebbcb8f05ba42760
hadryans <hadryansalles00@gmail.com>
2020-02-27 03:21:11 -0300

    normalized difference

--- source/compare.py
+++ source/compare.py
@@ -19,8 +19,8 @@ for img in gt :
             dif = cv2.subtract(o, d)
             b, g, r = cv2.split(dif)
             if cv2.countNonZero(b) != 0 or cv2.countNonZero(g) != 0 or cv2.countNonZero(r) != 0 :
-                abs_dif = (sum(sum(r)) + sum(sum(g)) + sum(sum(b))) / (255*o.shape[0]*o.shape[1]*o.shape[2])
-                tdif += abs_dif
-                print("error. images aren't equal", img, "difference absolute of", "%.3f" % abs_dif)
-print("complete. total difference", "%.3f" % tdif)
+                dif = (sum(sum(r)) + sum(sum(g)) + sum(sum(b))) / (255*o.shape[0]*o.shape[1]*o.shape[2])
+                tdif += dif
+                print("error. images aren't equal", img, "difference absolute of", "%.5f" % dif)
+print("complete. total difference", "%.5f" % tdif)
     
