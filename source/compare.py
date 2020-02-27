04d98e101bc1eed88938fa06b13a6ecbe4c61132
hadryans <hadryansalles00@gmail.com>
2020-02-27 03:05:52 -0300

    absolute difference of images

--- source/compare.py
+++ source/compare.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3.6
 import cv2
-import numpy as np
 import os
 
 gtp = "../pngs-gt/"
@@ -8,22 +7,19 @@ outp = "../pngs-out/"
 gt = os.listdir(gtp)
 out = os.listdir(outp)
 
-for i in range(len(gt)) :
-    if gt[i] != out[i] : 
-        print("error. missing file", gt[i]) 
-        exit()
-    o = cv2.imread(gtp+gt[i])
-    d = cv2.imread(outp+out[i])
-    if o.shape != d.shape : 
-        print("error. images with different shapes", gt[i])
-        exit()
-    dif = cv2.subtract(o, d)
-    b, g, r = cv2.split(dif)
-    if cv2.countNonZero(b) != 0 or cv2.countNonZero(g) != 0 or cv2.countNonZero(r) != 0 :
-        print("error. images aren't equal", gt[i], out[i])
-        os.system("open " + gtp+gt[i])
-        os.system("open " + outp+out[i])
-        exit()
-
-print("complete. all images are equal.")
+for img in gt :
+    if not out.__contains__(img) :
+        print("error. missing file", img) 
+    else :
+        o = cv2.imread(gtp+img)
+        d = cv2.imread(outp+img)
+        if o.shape != d.shape : 
+            print("error. images with different shapes", img)
+        else :    
+            dif = cv2.subtract(o, d)
+            b, g, r = cv2.split(dif)
+            if cv2.countNonZero(b) != 0 or cv2.countNonZero(g) != 0 or cv2.countNonZero(r) != 0 :
+                abs_dif = (sum(sum(r)) + sum(sum(g)) + sum(sum(b))) / (255*o.shape[0]*o.shape[1]*o.shape[2])
+                print("error. images aren't equal", img, "difference absolute of", "%.3f" % abs_dif)
+print("complete.")
     
