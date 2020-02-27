1150753ea0773d68965fe9d1fbb154a85feb3d83
hadryans <hadryansalles00@gmail.com>
2020-02-27 03:14:48 -0300

    total diff between image sets

--- source/compare.py
+++ source/compare.py
@@ -2,11 +2,11 @@
 import cv2
 import os
 
-gtp = "../pngs-gt/"
+gtp = "../pngs/"
 outp = "../pngs-out/"
 gt = os.listdir(gtp)
 out = os.listdir(outp)
-
+tdif = 0
 for img in gt :
     if not out.__contains__(img) :
         print("error. missing file", img) 
@@ -20,6 +20,7 @@ for img in gt :
             b, g, r = cv2.split(dif)
             if cv2.countNonZero(b) != 0 or cv2.countNonZero(g) != 0 or cv2.countNonZero(r) != 0 :
                 abs_dif = (sum(sum(r)) + sum(sum(g)) + sum(sum(b))) / (255*o.shape[0]*o.shape[1]*o.shape[2])
+                tdif += abs_dif
                 print("error. images aren't equal", img, "difference absolute of", "%.3f" % abs_dif)
-print("complete.")
+print("complete. total difference", "%.3f" % tdif)
     
