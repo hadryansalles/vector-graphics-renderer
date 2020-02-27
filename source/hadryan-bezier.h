a7ff5459616da14bd59095a316d9a084c35da45a
hadryans <hadryansalles00@gmail.com>
2020-02-27 07:17:35 -0300

    shortcut-tree organized

--- source/hadryan-bezier.h
+++ source/hadryan-bezier.h
@@ -1,3 +1,4 @@
+#pragma once
 
 #include "rvg-point.h"
 #include <vector>
---
+++ source/hadryan-shortcut-tree.cpp
@@ -0,0 +1,10 @@
+#include "hadryan-shortcut-tree.h"
+
+namespace rvg {
+    namespace driver {
+        namespace png {
+
+int tree_node::max_depth = 2;
+int tree_node::min_segments = 1;
+
+}}}
