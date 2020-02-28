41ead23f78f683da5f17b6790b25751a3310ef63
hadryans <hadryansalles00@gmail.com>
2020-02-28 04:55:28 -0300

    videos-out

--- source/video.sh
+++ source/video.sh
@@ -6,16 +6,16 @@ yf=0
 dx=1
 dy=1
 in=$1
-mkdir ../video
+mkdir ../video-data
 i=0
 for ((x=$xi; x<=$xf; x+=$dx)) do
-	luapp5.3 process.lua driver.png ../rvgs/$in.rvg ../video/$in-`printf %05d $i`.png -tx:$x $@ 
+	luapp5.3 process.lua driver.png ../rvgs/$in.rvg ../video-data/$in-`printf %05d $i`.png -tx:$x $@ 
 	((i++))
 done
 for ((y=$yi; y<=$yf; y+=$dy)) do
-	luapp5.3 process.lua driver.png ../rvgs/$in.rvg ../video/$in-`printf %05d $i`.png -ty:$y $@ 
+	luapp5.3 process.lua driver.png ../rvgs/$in.rvg ../video-data/$in-`printf %05d $i`.png -ty:$y $@ 
 	((i++))
 done
-ffmpeg -framerate 30 -i ../video/$in-%05d.png -y ../$in.mp4
-open ../$in.mp4
-rm -rf ../video
+ffmpeg -framerate 30 -i ../video-data/$in-%05d.png -y ../videos-out/$in.mp4
+open ../videos-out/$in.mp4
+rm -rf ../video-data
